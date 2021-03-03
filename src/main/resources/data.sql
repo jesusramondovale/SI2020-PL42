13df2e0b5f228ac2030fadb61db2ebc756410080
Jesús Ramón <uo261060@uniovi.es>
2021-03-03 10:49:03 +0100

    Data.sql y Schema.sql - v1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,70 @@
 --Datos para carga inicial de la base de datos
-delete from carreras;
-insert into carreras(id,inicio,fin,fecha,descr) values 
-	(100,'2016-10-05','2016-10-25','2016-11-09','finalizada'),
-	(101,'2016-10-05','2016-10-25','2016-11-10','en fase 3'),
-	(102,'2016-11-05','2016-11-09','2016-11-20','en fase 2'),
-	(103,'2016-11-10','2016-11-15','2016-11-21','en fase 1'),
-	(104,'2016-11-11','2016-11-15','2016-11-22','antes inscripcion');
+
+delete from socio;
+delete from tecnico;
+delete from licencia;
+delete from recibo;
+delete from asamblea;
+
+
+
+insert into socio(id, DNI , nombre , sexo  , fecha_nac, club , num_licencia , IBAN , cuota , DNI_tutor, DNI_tecnico) values 
+	
+        (1, 00000000A ,'Marta Perez Lopez','M','1965-12-06', 0000001, 'ES6000491500051234567892' , 250 ,  , 73264894D),
+        (2, 00000001B ,'Jorge Martinez Sanchez','H','1974-04-01', 0000002, 'ES6000446350056789567892' , 275 ,  , 4523194H),
+        (3, 00000002C  ,'Jesus Ramon Ramon','H','1998-06-04', 0000003, 'ES6000491475131245107892' , 220 ,  , 56410347R),
+        (4, 00000003D ,'Diego Gonzalez Prieto','H','1985-09-10', 0000004, 'ES4984491450151264037954' , 350 ,  , c); 
+
+
+
+insert into tecnico (id_tecnico , DNI_tecnico , nombre_tecnico, num_licencia) values 
+
+       (1, 73264894D, 'Rodrigo Nuñez Gavela', 696969), 
+       (2, 4523194H , 'Mario Mendez Martinez', 888888), 
+       (3, 56410347R, 'Maria Rodriguez Prieto', 123123),
+       (4, 65138741S, 'Luis Ramon Guerrero', 987987); 
+
+
+
+
+insert into licencia (id_licencia , num_licencia, estado) values 
+
+       (1, 0000001, 'Pagada'), 
+       (2, 0000002, 'Pendiente'),
+       (3, 0000003, 'Pendiente'),
+       (4, 0000004, 'Pagada'), 
+       (5, 696969 , 'Pagada'),
+       (6, 888888 , 'Pagada'),
+       (7, 123123 , 'Pagada'),
+       (8, 987987 , 'Pagada');
+
+
+
+insert into recibo (id_recibo, num_recibo , concepto , fecha_valor , fecha_emision , datos_socio ) values 
+     
+      (1, 000001, 'Pago matricula total', '2020-05-17', 2020-05-22', 'Marta Perez Lopez'),
+      (2, 000002, 'Pago matricula parcial', '2020-11-07', 2020-11-13', 'Jorge Martinez Sanchez'),
+      (3, 000003, 'Pago matricula', '2021-01-02', 2021-01-06', 'Jesus Ramon Ramon'),
+      (4, 000004, 'Reserva', '2021-02-14', 2021-02-21', 'Diego Gonzalez Prieto'); 
+
+
+
+insert into asamblea (id_asamblea , tipo , orden_dia , fecha , hora) values 
+
+      (1, 'Ordinaria', '1.Limpieza Ventanas 2.Limpieza Escalera', '2021-03-15', 17:00:00),
+      (2, 'Extraordinaria', '1.Arreglo luces 2.Limpieza Ventanas 3.Sacar basura', '2021-04-20', 12:00:00),
+      (3, 'Ordinaria', '1.Eleccion presidente 2.Arreglo caldera', '2021-03-15', 15:15:00),
+      (4, 'Extraordinaria', '1.Reparto tareas limpieza 2.Contratacion portero', '2021-03-23', 16:30:00); 
+
+
+
+
+ 
+
+    
+      
+
+
+
+
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,40 @@
 --(en este caso en cada una de las aplicaciones (tkrun y descuento) se usa solo una tabla, por lo que no hace falta)
 
 --Para giis.demo.tkrun:
-drop table Carreras;
-create table Carreras (id int primary key not null, inicio date not null, fin date not null, fecha date not null, descr varchar(32), check(inicio<=fin), check(fin<fecha));
+
+drop table socio;
+drop table tecnico;
+drop table licencia;
+drop table recibo;
+drop table asamblea;
+drop table reserva;
+
+
+create table Socio (id_socio int primary key not null, DNI varchar(12) not null, nombre varchar(32) not null, sexo varchar(1) not null, fecha_nac date not null, club varchar (12) not null, 
+                    num_licencia int not null, IBAN int not null, cuota int not null, DNI_tutor varchar (12), DNI_tecnico varchar(12),
+                    foreign key(DNI_tecnico) references tecnico(id_tecnico));  
+
+
+
+create table tecnico (id_tecnico int primary key not null, DNI_tecnico varchar (12) not null, nombre_tecnico varchar (32) not null, num_licencia int not null); 
+
+
+
+create table licencia (id_licencia int primary key not null, num_licencia int not null, estado varchar(12) not null);
+
+
+
+create table recibo (id_recibo int primary key not null, num_recibo int not null, concepto varchar(32) not null, fecha_valor date not null, fecha_emision date not null, datos_socio int not null,
+                    foreign key (datos_socio) references socio(id_socio)); 
+
+
+
+create table asamblea (id_asamblea int primary key not null, tipo varchar(12) not null, orden_dia varchar(120) not null, fecha date not null, hora time not null); 
+
+
+
+create table reserva (id_reserva primary key not null, id_socio int not null, tipo_instalacion varchar(32) not null, fecha date not null, hora_ini time not null, hora_fin time not null,
+                      foreign key (id_socio) references socio(id_socio)); 
+
+     
 
