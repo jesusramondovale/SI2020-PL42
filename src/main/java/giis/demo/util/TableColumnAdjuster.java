694c2aa2e73e4da7c1499650c1116757ad41d24a
Jesús Ramón <uo261060@uniovi.es>
2021-03-09 11:54:27 +0100

    AsambleasView + InstalacionesView - v1

--- src/main/java/giis/demo/util/TableColumnAdjuster.java
+++ src/main/java/giis/demo/util/TableColumnAdjuster.java
@@ -353,6 +353,10 @@ public class TableColumnAdjuster implements PropertyChangeListener, TableModelLi
 	 */
 	class ColumnAction extends AbstractAction
 	{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 		private boolean isSelectedColumn;
 		private boolean isAdjust;
 
