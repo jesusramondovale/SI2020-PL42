3e275e492dd15d7651811e43ef80180025ca8006
Pelayo <Pelayo@192.168.1.34>
2021-05-04 22:00:04 +0200

    Historia 14088

--- src/main/java/giis/demo/proyectoClub/model/ReciboCuotaModel.java
+++ src/main/java/giis/demo/proyectoClub/model/ReciboCuotaModel.java
@@ -1,13 +1,20 @@
 package giis.demo.proyectoClub.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import giis.demo.proyectoClub.DTO.ReciboDisplayDTO;
 import giis.demo.proyectoClub.DTO.SociosDisplayDTO;
 import giis.demo.util.Database;
 
 public class ReciboCuotaModel {
 	
 	private Database db = new Database();
+	ArrayList lista= new ArrayList();
 	
 	public List<SociosDisplayDTO> getSocios() {
 		String sql = "SELECT idSocio FROM socio";
@@ -43,6 +50,30 @@ public class ReciboCuotaModel {
 			String numCuenta) {
 		String sql = "INSERT INTO recibos (nRecibo, fechaValor, fechaEmision, concepto, importe, socio, numCuenta) values (?,?,?,?,?)";
 		db.executeUpdate(sql, nRecibo, fValor, fEmision, concepto, importe, socio, numCuenta);
+	}
+	
+	public ArrayList getFechaEmision(){
+		
+		Connection dbConnection=null;
+		PreparedStatement preparedStatement=null;
+		
+		String sql = "Select fechaEmision FROM recibos";
+		try {
+			dbConnection=db.getConnection();
+			preparedStatement=dbConnection.prepareStatement(sql);
+			ResultSet result=preparedStatement.executeQuery();
+			while(result.next()) {
+				String fecha= result.getString("fechaEmision");
+				ReciboDisplayDTO r = new ReciboDisplayDTO(fecha);
+				lista.add(r);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return lista;
+		
 	}
 
 }
